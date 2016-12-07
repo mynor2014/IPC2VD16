d8cfbafcfa889ccab3f63ea92c4192fc51389d01
Unknown <201403520@ingenieria.usac.edu.gt>
2016-12-07 09:30:52 -0600

    Multi2
    commit 5

--- ht1/ht1/Multi.vb
+++ ht1/ht1/Multi.vb
@@ -1,7 +1,7 @@
 ﻿Public Class Multi
     Public Function Multiplicar(num1 As Integer, num2 As Integer) As Integer
         Dim resultado As Integer = 0
-        resultado = num1 * num2
+        resultado = num2 * num1
         Return resultado
     End Function
 End Class
