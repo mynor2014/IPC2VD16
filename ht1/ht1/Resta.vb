807a003d6b82a607ba564d75c1041190a97d5880
Unknown <201403520@ingenieria.usac.edu.gt>
2016-12-07 08:33:46 -0600

    Resta
    resta operaciones

---
+++ ht1/ht1/Resta.vb
@@ -0,0 +1,7 @@
+﻿Public Class Resta
+    Public Function Resta(num1 As Integer, num2 As Integer) As Integer
+        Dim resultado As Integer = 0
+        resultado = num1 - num2
+        Return resultado
+    End Function
+End Class
